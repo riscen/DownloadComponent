71eaf99d0e5f24ff2873d2e89ff0ab4ff643a381
riscen <riscen_@hotmail.com>
2018-10-25 12:48:24 -0500

    Update
    Update transition behaviour

---
+++ src/constants/util.js
@@ -0,0 +1,14 @@
+export const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Spr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
--- src/js/download/DownloadFilter.jsx
+++ src/js/download/DownloadFilter.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import MonthPicker from "react-month-picker";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import { ChevronDown, ChevronUp } from "react-feather";
-
 import { formatDate, parseDate } from "react-day-picker/moment";
+import { MONTHS } from "../../constants/util";
 
 import "react-month-picker/css/month-picker.css";
 import "react-day-picker/lib/style.css";
@@ -13,7 +13,7 @@ class DownloadFilter extends Component {
     super(props);
 
     this.state = {
-      hideFilters: true,
+      showFilters: false,
       user: "",
       uploadBy: "",
       timePeriod: {
@@ -24,21 +24,6 @@ class DownloadFilter extends Component {
       showFromDate: false,
       showToDate: false
     };
-
-    this.months = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Spr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
-    ];
 
     this.handleFilterViewChange = this.handleFilterViewChange.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
@@ -60,24 +45,40 @@ class DownloadFilter extends Component {
     };
   }
 
+  mapDate(date) {
+    if (date) {
+      const { year, month } = date;
+      return `${MONTHS[month - 1]}/${year}`;
+    }
+    return "";
+  }
+
   handleFilterViewChange() {
-    if (!this.state.hideFilters) {
-      this.setState({
-        hideFilters: true,
-        user: "",
-        uploadBy: "",
-        timePeriod: {
-          from: null,
-          to: null
+    var growDiv = document.getElementsByClassName("download-filter-grow")[0];
+    if (this.state.showFilters) {
+      this.setState(
+        {
+          showFilters: false,
+          user: "",
+          uploadBy: "",
+          timePeriod: {
+            from: null,
+            to: null
+          },
+          uploadDate: null,
+          showFromDate: false,
+          showToDate: false
         },
-        uploadDate: null,
-        showFromDate: false,
-        showToDate: false
-      });
-    } else
+        () => this.props.handleFilterDataChange(this.getFilters(null, null, null))
+      );
+      growDiv.style.height = 0;
+    } else {
       this.setState({
-        hideFilters: false
+        showFilters: true
       });
+      const wrapper = document.querySelector(".download-filter-wrapper");
+      growDiv.style.height = wrapper.clientHeight + "px";
+    }
   }
 
   handleTextChange(event) {
@@ -94,7 +95,7 @@ class DownloadFilter extends Component {
   }
 
   handleFromDateChange(year, month) {
-    console.log(year, this.months[month - 1]);
+    console.log(year, MONTHS[month - 1]);
     this.hideFromDate();
     this.setState({
       timePeriod: {
@@ -104,16 +105,8 @@ class DownloadFilter extends Component {
     });
   }
 
-  mapDate(date) {
-    if (date) {
-      const { year, month } = date;
-      return `${this.months[month - 1]}/${year}`;
-    }
-    return "";
-  }
-
   handleToDateChange(year, month) {
-    console.log(year, this.months[month - 1]);
+    console.log(year, MONTHS[month - 1]);
     this.hideToDate();
     this.setState({
       timePeriod: {
@@ -158,21 +151,9 @@ class DownloadFilter extends Component {
 
   componentDidMount() {
     this.props.handleFilterDataChange(this.getFilters(null, null, null));
-    //feather.replace(); //Pinta iconos en el menu
   }
 
   render() {
-    const { from, to } = this.state.timePeriod;
-    const modifiers = { start: from, end: to };
-    let pickerLang = {
-        from: "From",
-        to: "To"
-      },
-      mrange = { from: { year: 2014, month: 8 }, to: { year: 2015, month: 5 } };
-    let makeText = m => {
-      if (m && m.year && m.month) return pickerLang.months[m.month - 1] + ". " + m.year;
-      return "?";
-    };
     let yearRange = [],
       date = new Date();
     for (let i = 0; i <= 20; i++) {
@@ -185,92 +166,88 @@ class DownloadFilter extends Component {
       ? this.state.timePeriod.to
       : { year: date.getFullYear(), month: date.getMonth() + 1 };
     return (
-      <div className={this.state.hideFilters ? "download-filter" : "download-filter active"}>
+      <div className={this.state.showFilters ? "download-filter active" : "download-filter"}>
         <div className="download-filter-title" onClick={this.handleFilterViewChange}>
           <span>Filters</span>
-          {this.state.hideFilters ? <ChevronDown /> : <ChevronUp />}
+          {this.state.showFilters ? <ChevronUp /> : <ChevronDown />}
         </div>
-        <div
-          className={
-            this.state.hideFilters
-              ? "download-filter-container close"
-              : "download-filter-container open"
-          }
-        >
-          <div className="download-filter-field-container download-filter-user-container">
-            <label className="download-filter-label">User: </label>
-            <input
-              type="text"
-              className="download-filter-input-field"
-              placeholder="Username"
-              name="user"
-              value={this.state.textUserFilter}
-              onChange={this.handleTextChange}
-            />
-          </div>
+        <div className="download-filter-grow">
+          <div className="download-filter-wrapper">
+            <div className="download-filter-field-container download-filter-user-container">
+              <label className="download-filter-label">User: </label>
+              <input
+                type="text"
+                className="download-filter-input-field"
+                placeholder="Username"
+                name="user"
+                value={this.state.user}
+                onChange={this.handleTextChange}
+              />
+            </div>
 
-          <div className="download-filter-field-container download-filter-time-period-container">
-            <label className="download-filter-label">Time period: </label>
-            <div className="InputFromTo">
-              <div className="download-from-date el-inline">
-                <MonthPicker
-                  show={this.state.showFromDate}
-                  years={yearRange}
-                  value={valueFromDate}
-                  lang={this.months}
-                  onChange={this.handleFromDateChange}
-                >
-                  <input
-                    type="text"
-                    placeholder="From"
-                    onFocus={this.showFromDate}
-                    value={this.mapDate(this.state.timePeriod.from)}
-                    readOnly
-                  />
-                </MonthPicker>
-              </div>
-              <div className="download-to-date el-inline">
-                <MonthPicker
-                  show={this.state.showToDate}
-                  years={yearRange}
-                  value={valueToDate}
-                  lang={this.months}
-                  onChange={this.handleToDateChange}
-                >
-                  <input
-                    type="text"
-                    placeholder="To"
-                    onFocus={this.showToDate}
-                    value={this.mapDate(this.state.timePeriod.to)}
-                    readOnly
-                  />
-                </MonthPicker>
+            <div className="download-filter-field-container download-filter-time-period-container">
+              <label className="download-filter-label">Time period: </label>
+              <div className="InputFromTo">
+                <div className="download-from-date el-inline">
+                  <MonthPicker
+                    show={this.state.showFromDate}
+                    years={yearRange}
+                    value={valueFromDate}
+                    lang={this.months}
+                    onChange={this.handleFromDateChange}
+                  >
+                    <input
+                      type="text"
+                      placeholder="From"
+                      onFocus={this.showFromDate}
+                      value={this.mapDate(this.state.timePeriod.from)}
+                      readOnly
+                    />
+                  </MonthPicker>
+                </div>
+                <div className="download-to-date el-inline">
+                  <MonthPicker
+                    show={this.state.showToDate}
+                    years={yearRange}
+                    value={valueToDate}
+                    lang={this.months}
+                    onChange={this.handleToDateChange}
+                  >
+                    <input
+                      type="text"
+                      placeholder="To"
+                      onFocus={this.showToDate}
+                      value={this.mapDate(this.state.timePeriod.to)}
+                      readOnly
+                    />
+                  </MonthPicker>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="download-filter-field-container download-filter-uploaded-by-container">
-            <label className="download-filter-label">Uploaded by: </label>
-            <input
-              type="text"
-              className="download-filter-input-field"
-              placeholder="Username"
-              name="uploadBy"
-              value={this.state.textUploaderFilter}
-              onChange={this.handleTextChange}
-            />
-          </div>
-
-          <div className="download-filter-field-container download-filter-upload-date-container">
-            <label className="download-filter-label">Upload date: </label>
-            <div>
-              <DayPickerInput
-                formatDate={formatDate}
-                parseDate={parseDate}
-                format="DD/MM/YYYY"
-                placeholder={formatDate(new Date(), "DD/MM/YYYY")}
-                onDayChange={this.handleDateChange}
+            <div className="download-filter-field-container download-filter-uploaded-by-container">
+              <label className="download-filter-label">Uploaded by: </label>
+              <input
+                type="text"
+                className="download-filter-input-field"
+                placeholder="Username"
+                name="uploadBy"
+                value={this.state.uploadBy}
+                onChange={this.handleTextChange}
               />
+            </div>
+
+            <div className="download-filter-field-container download-filter-upload-date-container">
+              <label className="download-filter-label">Upload date: </label>
+              <div>
+                <DayPickerInput
+                  formatDate={formatDate}
+                  parseDate={parseDate}
+                  format="DD/MM/YYYY"
+                  placeholder={formatDate(new Date(), "DD/MM/YYYY")}
+                  onDayChange={this.handleDateChange}
+                />
+              </div>
             </div>
           </div>
         </div>
